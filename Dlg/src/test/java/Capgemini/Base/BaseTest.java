9c2f76a2105530853003c705be6915383ed7e59d
yedukondalu105 <60851538+yedukondalu105@users.noreply.github.com>
2020-05-12 16:10:28 +0530

    Update BaseTest.java

--- Dlg/src/test/java/Capgemini/Base/BaseTest.java
+++ Dlg/src/test/java/Capgemini/Base/BaseTest.java
@@ -43,8 +43,8 @@ public class BaseTest {
 		  driver=new ChromeDriver();
 	 }
 	 else if(env.prop.getProperty("OS").equalsIgnoreCase("linux")) {
-		  System.setProperty("webdriver.gecko.driver", "BrowserDrivers/geckodriver");
-		  driver=new FirefoxDriver();
+		  System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriver");
+		  driver=new ChromeDriver();
 	 }
 	  
 	  
